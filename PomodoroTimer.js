27546048351fcf1586bce0b4bd30ad7f8795b37b
Jason Sullivan <jasoncsullivan@gmail.com>
2021-02-01 01:34:23 -0500

    colors

--- PomodoroTimer.js
+++ PomodoroTimer.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { StyleSheet, Text, View, Button } from "react-native";
 
 import { vibrate } from "./utils";
@@ -6,27 +6,34 @@ import { vibrate } from "./utils";
 export default function PomodoroTimer() {
   const [timeLeft, setTimeLeft] = useState(null);
   const [isRunning, setIsRunning] = useState(false);
+  const [isWorkMode, setIsWorkMode] = useState(true);
 
   const intervalRef = useRef();
 
-  const decrementTimer = (isWorkMode) =>
+  const decrementTimer = () =>
     setTimeLeft((prevState) => {
       if (prevState - 1 == 0) {
         vibrate();
         clearInterval(intervalRef.current);
         intervalRef.current = null;
-        onStartTimer(!isWorkMode);
+        setIsWorkMode((prevState) => !prevState);
       }
       return prevState - 1;
     });
 
-  const onStartTimer = (isWorkMode) => {
-    setIsRunning(true);
+  useEffect(() => {
     setTimeLeft(60 * (isWorkMode ? 0.1 : 5));
 
     intervalRef.current = setInterval(() => {
-      decrementTimer(isWorkMode);
+      decrementTimer();
     }, 1000);
+
+    return () => clearInterval(intervalRef.current);
+  }, [isRunning, isWorkMode]);
+
+  const onStartTimer = () => {
+    console.log("starting timer", isWorkMode);
+    setIsRunning(true);
   };
 
   const onStopTimer = () => {
@@ -34,6 +41,7 @@ export default function PomodoroTimer() {
     setTimeLeft(null);
     clearInterval(intervalRef.current);
     intervalRef.current = null;
+    setIsWorkMode(true);
   };
 
   const formatTimeLeft = (tl) => {
@@ -42,9 +50,17 @@ export default function PomodoroTimer() {
 
   return (
     <View>
-      <Text style={styles.timerInWorkMode}>
-        {isRunning ? formatTimeLeft(timeLeft) || "-" : "-"}
-      </Text>
+      {isWorkMode ? (
+        <Text style={styles.timerInWorkMode}>
+          Work!
+          {isRunning ? formatTimeLeft(timeLeft) || "-" : "-"}
+        </Text>
+      ) : (
+        <Text style={styles.timerNotInWorkMode}>
+          Break!
+          {formatTimeLeft(timeLeft)}
+        </Text>
+      )}
 
       {!isRunning && (
         <Button
@@ -64,6 +80,11 @@ export default function PomodoroTimer() {
 const styles = StyleSheet.create({
   timerInWorkMode: {
     color: "green",
+    fontSize: 30,
+    textAlign: "center",
+  },
+  timerNotInWorkMode: {
+    color: "red",
     fontSize: 30,
     textAlign: "center",
   },
